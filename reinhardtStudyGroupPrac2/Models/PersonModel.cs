63a3f9f7af2f2783db60830d36e712dc77a1c489
u24587878 <reinhardt.coetzee2005@gmail.com>
2025-03-10 15:08:02 +0200

    Reused Model

---
+++ reinhardtStudyGroupPrac2/Models/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace INF272Lecture4v1.Models
+{
+    public class PersonModel
+    {
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Current Age")]
+        public int Age { get; set; } = 0;
+
+        [Display(Name = "Living Status")]
+        public bool IsAlive { get; set; } = true;
+    }
+}
